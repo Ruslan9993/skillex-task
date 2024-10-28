e00ae63f0290dc0d2ed86af9ee7eeb1b331ecbf6
Ruslan Simonyan <ruslan.simonyan@bostongene.com>
2024-10-28 17:32:41 +0400

    added webpack clean and html plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const path = require("path");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 let mode = "development";
 let target = "web"
@@ -14,6 +17,7 @@ module.exports = {
     target: target,
 
     output: {
+        path: path.resolve(__dirname, "dist"),
         assetModuleFilename: "images/[hash][ext][query]"
     },
 
@@ -43,7 +47,13 @@ module.exports = {
         extensions: [".js", ".jsx"]
     },
 
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+        template: "./src/index.html",
+    })
+    ],
 
     devServer: {
         static: "./dist",
