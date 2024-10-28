b0154c1c012e695d6d7f11c28720754c3c51eeff
Ruslan Simonyan <ruslan.simonyan@bostongene.com>
2024-10-28 16:46:32 +0400

    added css and scss support and hotreloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,40 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 let mode = "development";
+let target = "web"
+
 
 if (process.env.NODE_ENV === "production") {
     mode = "production";
+    target = "browserslist";
 }
 
 module.exports = {
     mode: mode,
+    target: target,
 
     devtool: "source-map",
     module: {
         rules: [
+            {
+                test: /\.(s[ac]|c)ss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
                 }
-            }
+            },
+
         ]
     },
 
+    plugins: [new MiniCssExtractPlugin()],
+
     devServer: {
         static: "./dist",
+        hot: true
     }
 }
